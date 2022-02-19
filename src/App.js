f386f2c5661cea94fec1b6e75baf84011f3f0a51
Mahir Absar <mahir.mohsin@northsouth.edu>
2022-02-19 20:24:43 +0600

    authentication added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import styled from 'styled-components'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
 // pages
 import Home from './pages/Home'
 import Login from './pages/Login'
 import CreateBlog from './pages/CreateBlog'
+import { auth } from './firebase-config'
+import { signOut } from 'firebase/auth'
 function App() {
+  const [isAuth, setIsAuth] = useState(false)
+  const logout = () => {
+    signOut(auth).then(() => {
+      localStorage.clear()
+      window.location.pathname = '/login'
+    })
+  }
   return (
     <div>
       <Router>
         <Wrapper>
           <Link to='/'>Home</Link>
           <Link to='/create'>Create Blog</Link>
-          <Link to='/login'>Login</Link>
+          {!isAuth ? (
+            <Link to='/login'>Login</Link>
+          ) : (
+            <button onClick={logout}>Log Out</button>
+          )}
         </Wrapper>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/create' element={<CreateBlog />} />
-          <Route path='/login' element={<Login />} />
+          <Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
         </Routes>
       </Router>
     </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { auth, provider } from '../firebase-config'
+import { signInWithPopup } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+function Login({ setIsAuth }) {
+  const navigate = useNavigate()
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem('isAuth', true)
+      setIsAuth(true)
+      navigate('/')
+    })
+  }
 
-function Login() {
-  return <div>Login</div>
+  return (
+    <div>
+      <h1 className='login_text'>Sign In With Google</h1>
+      <button className='btn' onClick={signInWithGoogle}>
+        Sign In
+      </button>
+    </div>
+  )
 }
 
 export default Login
